eea38f57d94d962b8bbc57bb383fe8bd5d3b891a
Vasiliy <vasbuchka@gmail.com>
2025-01-15 19:05:00 +0000

    dz 2.1

--- Python/HillelStart.py
+++ Python/HillelStart.py
@@ -1,2 +1,11 @@
-print('Hello world!')
+number = int(input("Введіть 4-значне число: "))
 
+a = number // 1000
+b = number % 1000 // 100
+c = number % 100 // 10
+d = number % 10 // 1
+
+print(a)
+print(b)
+print(c)
+print(d)
