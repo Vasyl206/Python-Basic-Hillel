b461fc32256092fab286e228db8fce1f3ce7015c
Vasiliy <vasbuchka@gmail.com>
2025-01-17 20:18:16 +0000

    DZ 3.2

--- Python/Dz 3.2.py
+++ Python/Dz 3.2.py
@@ -1,10 +1,15 @@
 # lst = [12, 3, 4, 10]
 # my_lst = [lst[-1]] + lst[:-1]
 # print(my_lst)
+
 # lst = [1]
 # my_lst = [lst[-1]] + lst[:-1]
 # print(my_lst)
-lst = []
-my_lst = [lst[-1]] + lst[:-1]
-print(my_lst)
-# [12, 3, 4, 10, 8] => [8, 12, 3, 4, 10
+
+# lst = []
+# my_lst = lst[:] + lst[:]
+# print(my_lst)
+
+# lst = [12, 3, 4, 10, 8]
+# my_lst = [lst[-1]] + lst[:-1]
+# print(my_lst)
