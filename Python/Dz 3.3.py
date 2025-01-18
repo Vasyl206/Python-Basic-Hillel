2228965a8a1618fa19cecafd155665d3c8f9990e
Vasiliy <vasbuchka@gmail.com>
2025-01-18 12:44:39 +0000

    DZ 3.2

--- Python/Dz 3.3.py
+++ Python/Dz 3.3.py
@@ -1,19 +1,13 @@
-# lst = [1, 2, 3, 4, 5, 6]
-# my_lst = lst[0:3], lst[3:888]
-# print(my_lst)
+#lst = [1, 2, 3, 4, 5, 6]
 
-# lst = [1, 2, 3]
-# my_lst = lst[0:2], lst[2:888]
-# print(my_lst)
+#lst = [1, 2, 3]
 
-# lst = [1, 2, 3, 4, 5]
-# my_lst = lst[0:3], lst[3:888]
-# print(my_lst)
+#lst = [1, 2, 3, 4, 5]
 
-# lst = [1]
-# my_lst = lst[:1], lst[1:]
-# print(my_lst)
+#lst = [1]
 
-# lst = []
-# my_lst = lst[:1], lst[1:]
-# print(my_lst)
+#lst = []
+
+my_lst = (len(lst) + 1) // 2
+my_super_lst = [lst[:my_lst], lst[my_lst:]]
+print(my_super_lst)
