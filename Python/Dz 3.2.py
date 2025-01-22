de135dd655ae932838a1111656daccd266b477ad
Vasiliy <vasbuchka@gmail.com>
2025-01-22 20:51:22 +0000

    DZ 4.2

--- Python/Dz 3.2.py
+++ Python/Dz 3.2.py
@@ -1,6 +1,18 @@
-#lst = [12, 3, 4, 10]
-#lst = [1]
+#lst = [0, 1, 7, 2, 4, 8] #=> (0 + 7 + 4) * 8 = 88
+
+#lst = [1, 3, 5]
+
+#lst = [6]
+
 #lst = []
-lst = [12, 3, 4, 10, 8]
-my_lst = lst[-1:] + lst[:-1]
-print(my_lst)
+
+if sum(lst) == 0:
+    print(sum(lst))
+else:
+    my_lst = lst[::2]
+    uno = sum(my_lst) * lst[-1]
+    print(uno)
+
+
+
+
