7c89a686e3981b6f890c4e6a0df118012d83078b
Vasiliy <vasbuchka@gmail.com>
2025-02-02 20:59:34 +0000

    DZ 7.2

--- Python/Dz 3.2.py
+++ Python/Dz 3.2.py
@@ -1,17 +1,14 @@
-seconds = int(input("Введіть число від 0 і до 8640000: "))
 
-minut, sec = divmod(seconds, 60)
-hour, minut = divmod(minut, 60)
-day, hour = divmod(hour, 24)
+def correct_sentence(text):
+    text = text.capitalize()
+    if not text.endswith("."):
+        text += "."
+    return text
 
-if day % 10 == 1 and day % 100 != 11:
-    day_str = f"{day} день"
-elif 2 <= day % 10 <= 4 and (day % 100 < 10 or day % 100 >= 20):
-    day_str = f"{day} дні"
-else:
-    day_str = f"{day} днів"
+text = str(input(": "))
 
-print(f"{day_str} {hour:02}:{minut:02}:{sec:02}")
+corrected_text = correct_sentence(text)
+print(corrected_text)
 
 
 
