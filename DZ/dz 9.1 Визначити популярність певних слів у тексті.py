7afe0d32e89c2e1663aef48c58cd1ff26cb708b1
Vasiliy <vasbuchka@gmail.com>
2025-02-08 19:45:39 +0000

    DZ 9.1

--- DZ/dz 9.1 Визначити популярність певних слів у тексті.py
+++ DZ/dz 9.1 Визначити популярність певних слів у тексті.py
@@ -15,7 +15,9 @@ def popular_words (text, words):
     text_2 = text_1.split() #разбивает строку на список слов, используя пробел как разделитель
     text_count = Counter(text_2) #считает каждое слово
     return {word: text_count.get(word, 0) for word in words}
-print(popular_words("When I was One I had just begun When I was Two I was nearly new", ['i', 'was', 'three', 'near']))
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
 
 
 
