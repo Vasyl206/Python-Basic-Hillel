597d78642ecefcc90aa9566f21e93901a236eba5
Vasiliy <vasbuchka@gmail.com>
2025-02-08 19:44:04 +0000

    DZ 9.2

--- DZ/dz 9.2. Різниця між числами.py
+++ DZ/dz 9.2. Різниця між числами.py
@@ -8,4 +8,8 @@ def difference(*numb):
     numb_good = round(max(numb) - min(numb), 2) #round округляет с 12.399999 до 12.4
     return numb_good
 
-print(difference())
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
