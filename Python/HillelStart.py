af7f863a74ab2b4f6ba19308886185ed7cfacca9
Vasiliy <vasbuchka@gmail.com>
2025-02-08 14:22:26 +0000

    DZ 8.1

--- Python/HillelStart.py
+++ Python/HillelStart.py
@@ -1,5 +1,9 @@
 
+def add_one(some_list):
+    wor = "".join(map(str, some_list))  # Преобразуем числа в строку
+    firstre_1 = int(wor)  # Преобразуем строку в число
+    sakond_2 = firstre_1 + 1  # Добавляем 1
+    tree = list(str(sakond_2))  # Преобразуем результат обратно в строку, потом в список
+    return tree
 
-month = set(["jan", "Feb", "march"])
-for m in month:
-    print(m, hash(month))
+print(add_one([1, 2, 3, 4]))
