61f831ee898f97fe4183d87c3237df6fe4c2fe44
Vasiliy <vasbuchka@gmail.com>
2025-02-08 16:18:14 +0000

    DZ 9.1

---
+++ DZ/dz 9.1 Визначити популярність певних слів у тексті.py
@@ -0,0 +1,26 @@
+
+#popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+# ['i', 'was', 'three', 'near'])
+# { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }
+
+# Сделать все слова в lower
+# Если слово не найдено нужно его вернуть с 0 значением
+# Посчитать каждое слово
+
+from collections import Counter
+
+
+def popular_words (text, words):
+    text_1 = text.lower()
+    text_2 = text_1.split() #разбивает строку на список слов, используя пробел как разделитель
+    text_count = Counter(text_2) #считает каждое слово
+    return {word: text_count.get(word, 0) for word in words}
+print(popular_words("When I was One I had just begun When I was Two I was nearly new", ['i', 'was', 'three', 'near']))
+
+
+
+
+
+
+
+
--- Python/HillelStart.py
+++ Python/HillelStart.py
@@ -1,9 +1,7 @@
+from collections import Counter
 
-def add_one(some_list):
-    wor = "".join(map(str, some_list))  # Преобразуем числа в строку
-    firstre_1 = int(wor)  # Преобразуем строку в число
-    sakond_2 = firstre_1 + 1  # Добавляем 1
-    tree = list(str(sakond_2))  # Преобразуем результат обратно в строку, потом в список
-    return tree
+text = "hello world hello python world python hello"
+words = text.split()  # Разбиваем текст на слова
+word_count = Counter(words)
 
-print(add_one([1, 2, 3, 4]))
+print(word_count)
