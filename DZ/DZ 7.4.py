3c7ae020d46e132275fb937bffefde881622069c
Vasiliy <vasbuchka@gmail.com>
2025-02-21 14:08:20 +0000

    Поменял название

--- DZ/DZ 7.4.py
+++
@@ -1,7 +0,0 @@
-def common_elements():
-    multiples_of_3 = {x for x in range(100) if x % 3 == 0}  # Числа, кратні 3
-    multiples_of_5 = {x for x in range(100) if x % 5 == 0}  # Числа, кратні 5
-
-    return multiples_of_3 & multiples_of_5
-
-print(common_elements())
