45d7226e23c6097d89fde51dfe82d1dd1376ccc8
Vasiliy <vasbuchka@gmail.com>
2025-02-21 14:37:21 +0000

    dz 11.3

---
+++ DZ/11.3. Перевірка на парність.py
@@ -0,0 +1,13 @@
+
+
+def is_even(number):
+    last_digit = int(str(number)[-1])
+    if last_digit in (0, 2, 4, 6, 8):
+        return True
+    else:
+        return False
+
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+print("OK")
